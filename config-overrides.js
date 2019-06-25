caaafa93d8987cdd5a584799b738c6a2f1896b7e
xiaolong <1433314824@qq.com>
2019-06-25 15:14:21 +0800

    搭建成功

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,12 @@
-const { override,addBadelPlugins,fixBabelImports} = require('customize-cra');
+const { override, addBabelPlugins, fixBabelImports } = require('customize-cra');
 module.exports = override(
-    ...addBabelPlugins(
-        [
-            "styled-jsx/babel",
-            {"plugins": ["styled-jsx-plugin-less"]}
-        ]
-    ),
-    fixBabelImports('import',{
-        libraryName:'antd-mobile',style:'css'
-    })
+  ...addBabelPlugins(
+    [
+      "styled-jsx/babel",
+      { "plugins": ["styled-jsx-plugin-less"] }
+    ]
+  ),
+  fixBabelImports('import', {
+    libraryName: 'antd-mobile', style: 'css'
+  })
 )
--- src/App.js
+++ src/App.js
@@ -1,11 +1,92 @@
 import React from 'react';
+import { TabBar } from 'antd-mobile';
+import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class TabBarExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+      fullScreen: false,
+    };
+  }
+
+
+  render() {
+    return (
+      <div style={ { position: 'fixed', height: '100%', width: '100%', top: 0 } }>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+          // hidden={this.state.hidden}
+        >
+          <TabBar.Item
+            title="首页"
+            key="Home"
+            icon={<span className="iconfont icon-home" /> }
+            selectedIcon={<span className="iconfont icon-home" /> }
+            selected={this.state.selectedTab === 'blueTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab',
+              });
+            }}
+            data-seed="logId"
+          >
+            1
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <span className="iconfont icon-gouwuche" />
+            }
+            selectedIcon={ <span className="iconfont icon-gouwuche" /> }
+            title="购物车"
+            key="Cart"
+            badge={'1'}
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab',
+              });
+            }}
+            data-seed="logId1"
+          >
+            2
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            title="Friend"
+            key="Friend"
+            dot
+            selected={this.state.selectedTab === 'greenTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'greenTab',
+              });
+            }}
+          >
+            3
+          </TabBar.Item>
+        </TabBar>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TabBarExample;
